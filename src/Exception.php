66548351987f535150d4809c3bd18c393100008a
Tamas Verten <tamas@verten.hu>
2023-12-20 10:19:47 +0100

    mods

--- src/Exception.php
+++ src/Exception.php
@@ -2,7 +2,7 @@
 namespace VtSoftware\Tools\PHPCurlClass;
 
 class Exception extends \Exception {
-  public function __construct($message, $code = 0, Throwable $previous = null) {
+  public function __construct($message, $code = 0, \Throwable $previous = null) {
     parent::__construct($message, $code, $previous);
   }
 }
